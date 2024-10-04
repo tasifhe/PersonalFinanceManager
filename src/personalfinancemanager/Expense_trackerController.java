fdd3c919dcc96c661d00591f158c8e43dc45647e
Tasif Hossain <tasifthe2002@gmail.com>
2024-10-05 01:46:12 +0600

    update v0.3

--- src/personalfinancemanager/Expense_trackerController.java
+++ src/personalfinancemanager/Expense_trackerController.java
@@ -4,6 +4,7 @@
  */
 package personalfinancemanager;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -18,8 +19,15 @@ import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -53,6 +61,10 @@ public class Expense_trackerController implements Initializable {
     
     // Observable list to hold the expenses
     private ObservableList<Expense> expenseList;
+    @FXML
+    private Button backButton;
+    @FXML
+    private VBox expenseTrackerController;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -145,5 +157,16 @@ public class Expense_trackerController implements Initializable {
             total += expense.getAmount();
         }
         return total;
+    }
+
+    @FXML
+    private void handleBack(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main_dashboard.fxml"));
+        Parent viewRoot = loader.load();
+
+        Scene scene = new Scene(viewRoot);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
